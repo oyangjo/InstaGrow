bd41ab00a37b145713608ee29b5bab27dfe82f25
Joseph Yang <josephyang98423@gmail.com>
2020-11-17 08:47:35 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='InstaGrow',
+      version='0.0',
+      description='Instagram Liker',
+      author='Joseph Yang',
+      author_email='joyangseph@gmail.com',
+      url='',
+      install_requires=['instapy', 'bs4']
+)
